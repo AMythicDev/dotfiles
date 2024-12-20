8665b27924b6cbd9edb703c497fc6e9d9f95d85c
Arijit Dey <arijit79@protonmail.com>
2024-12-21 00:30:02 +0530

    deploy new configs

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require "wezterm"
+
+local config = {}
+config.font = wezterm.font "Iosevka Nerd Font Mono"
+config.color_scheme = 'Tokyo Night'
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
